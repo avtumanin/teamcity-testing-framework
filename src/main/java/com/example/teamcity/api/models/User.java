2db2707e74f5ba22e3bfe25b9eb3315242f4c7f8
avtumanin <avtumanin@gmail.com>
2024-10-02 23:54:32 +0300

    add the first step in userCreatesBuildTypeTest, log4j.properties

--- src/main/java/com/example/teamcity/api/models/User.java
+++ src/main/java/com/example/teamcity/api/models/User.java
@@ -1,16 +1,17 @@
 package com.example.teamcity.api.models;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Data;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends BaseModel{
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User extends BaseModel {
     private String username;
     private String password;
 }
